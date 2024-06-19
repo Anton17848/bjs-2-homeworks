406b5fb5d5318a35749f12f1e7c64edd0b218006
Anton17848 <anton.kopenkin@icloud.com>
2024-06-19 16:35:40 +0300

    Zadaca-1-2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  let x1;
+  let x2;
+  let d = b * 2 - 4 * a * c;
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  } else if (d === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  }else if (d < o) {
+  }
+  return 
+  }
+  console.log(solveEquation());
   
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 }
+  
