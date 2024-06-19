5adfe781859b54a15e16a1d660f062232b685718
Anton17848 <anton.kopenkin@icloud.com>
2024-06-19 16:51:45 +0300

    Reschenie-zadachi-1-2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,23 +1,37 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
-  let arr = [];
-  let x1;
-  let x2;
-  let d = b * 2 - 4 * a * c;
+    let arr = [];
+    let x1;
+    let x2;
+    let d = b ** 2 - 4 * a * c;
 
-  if (d > 0) {
-    x1 = (-b + Math.sqrt(d)) / (2 * a);
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(x1, x2);
-  } else if (d === 0) {
-    x1 = -b / (2 * a);
-    arr.push(x1);
-  }else if (d < o) {
-  }
-  return 
-  }
-  console.log(solveEquation());
-  
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-}
-  
+    if (d > 0) {
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        arr.push(x1, x2);
+    } else if (d === 0) {
+        x1 = -b / (2 * a);
+        arr.push(x1);
+    }
+    return arr;
+    }
+    
+    console.log(solveEquation());
+
+    function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+        let per = parseInt(percent) / 100 / 12;
+        let con = parseInt(contribution);
+        let am = parseInt(amount);
+        if (isNaN(per) || per < 0) {
+            return `false`;
+        } else {
+                let s = am - con;
+                let n = countMonths;
+                let pay = s * (per + per / (((1 + per) ** n) - 1));
+                let totalAmount = (pay * n).toFixed(2);
+                console.log(totalAmount);
+                return +totalAmount;
+       }
+    }
+    
+
